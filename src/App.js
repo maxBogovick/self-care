f2e2f24850df77164ab159fb4e44be60b8b5a0f1
Maxim Bogovik <mbogovic@teamworkcommerce.com>
2023-05-27 11:06:48 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebcamImage from "./WebcamImage";
+import WebcamVideo from "./WebcamVideo";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <WebcamImage/>
+      <WebcamVideo/>
+    </div>
+  );
+}
+
+export default App;
